69685d2be153cd541085bbecaf814b4a7b33bcd5
Vughalla <epbelascuain@hotmail.com>
2024-05-02 14:02:32 -0300

    Incresed performance on memory usage
    Code refactor on function q1_memory, q2_memory and q3_memory in order to reduce memory usage during excecution.
    Added documentation and test examples on main notebook.

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -2,14 +2,30 @@ from typing import List, Tuple
 from datetime import datetime
 import pandas as pd
 
-
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
 
-    # Crea un dataframe de pandas a partir del json.
-    json_df = pd.read_json(file_path, lines=True)
+    df_concat = pd.DataFrame(columns=['date', 'username'])
+    for json_df in pd.read_json(file_path, lines=True, chunksize=100):
+        # Se extraen los key/value del json anidado y se guarda en un nuevo df.
+        user_df = json_df['user'].apply(pd.Series)
 
-    user_df = json_df['user'].apply(pd.Series)
-    json_df = pd.concat([json_df, user_df], axis=1)
+        # Se remueven todas las columnas excepto date para reducir la carga en memoria de la variable.
+        json_df = json_df['date']
+
+        # Se remueven todas las columnas excepto username para reducir la carga en memoria de la variable.
+        user_df = user_df['username']
+
+        # Ambos dataframes se unen nuevamente.
+        json_df = pd.concat([json_df, user_df], axis=1)
+
+        # Se agregan los registros del nuevo chuck, ya fitrando toda la data que no será usada, al df acumulador.
+        df_concat = pd.concat([df_concat, json_df])
+
+    json_df = df_concat
+
+    # Se elimina el df para liberar espacio en memoria.
+    del user_df
+    del df_concat
 
     # Convierte el formato de fecha a YYYY-MM-DD
     json_df['date'] = pd.to_datetime(json_df['date']).dt.date
@@ -20,14 +36,17 @@ def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
     # Remueve del dataframe original todos los registros donde la fecha sea diferente a aquellas donde hubo más actividad.
     json_df = json_df[json_df.date.isin(date_df['date'])]
 
+    # Se elimina el df para liberar espacio en memoria.
+    del date_df
+
     # Agrupa por fecha y usuario, y cuenta el número de tweets por fecha y usuario
-    grouped_df = json_df.groupby(['date', 'username']).size().reset_index(name='count')
+    json_df = json_df.groupby(['date', 'username']).size().reset_index(name='count')
 
     # Encuentra el usuario con más tweets por fecha
-    max_tweets_df = grouped_df.loc[grouped_df.groupby('date')['count'].idxmax()]
+    json_df = json_df.loc[json_df.groupby('date')['count'].idxmax()]
 
     # Ordena los resultados por fecha filtrando aquellas donde hayan menos actividad.
-    max_tweets_df = max_tweets_df.sort_values(by='date').tail(10)
+    json_df = json_df.sort_values(by='date').tail(10)
 
     # Recopila los resultados
-    return list(zip(max_tweets_df['date'], max_tweets_df['username']))
+    return list(zip(json_df['date'], json_df['username']))
